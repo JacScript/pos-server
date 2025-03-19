376be40271d93b93c6da80883ac8c2ec22be9fd7
JacScript <jacksonsylas59@gmail.com>
2025-03-19 19:19:35 +0300

    mc

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ application.use(express.json());
 
 // Middleware to parse URL-encoded data (optional)
 application.use(cors({
-    origin: "http://localhost:5173 ",
+    origin: ["http://localhost:5173 ", "http://127.0.0.1:5173"],
     credentials: true
 })
 ); 
